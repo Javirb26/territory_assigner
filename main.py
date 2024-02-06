1576dc9aaec324589122249491747f995dbec0a6
Javier Ballester <Javirb26@gmail.com>
2024-02-06 18:54:29 -0500

    added list of territories, a list of dictionaries holding each brothers info and imported python random module

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+
+# This program assigns a territory to a user(s) given a number of territories
+
+# Territory list
+territories = [
+    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
+]
+
+# example list of dictionaries holding names of brothers and their current as well as previous held territory
+brothers = [
+    {'name': 'brotherA',
+     'current_territory': None,
+     'previous_territory': None,
+     },
+    {'name': 'brotherB',
+     'current_territory': None,
+     'previous_territory': None,
+     },
+    {'name': 'brotherC',
+     'current_territory': None,
+     'previous_territory': None,
+     },
+]
